04ee0371b99d08a24daec9a34b64e2ba1d49b92b
ek9 <dev@ek9.co>
2016-05-22 13:33:07 +0200

    add vagrant home config to shell/profile.d

---
+++ .config/shell/profile.d/20-vagrant.sh
@@ -0,0 +1,4 @@
+## ek9/shell-dev-config - https://github.com/ek9/shell-dev-config
+## 20-vagrant.sh
+## Makes sure Vagrant is using ~/.local/share/vagrant for vagrant cache
+export VAGRANT_HOME="$HOME/.local/share/vagrant"
