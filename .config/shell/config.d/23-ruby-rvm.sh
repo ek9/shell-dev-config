0638bd87cb7963ee6157372b37fcc1a5ba8c2817
ek9 <dev@ek9.co>
2018-03-26 21:11:10 +0100

    cleanup ruby rvm alias

--- .config/shell/config.d/23-ruby-rvm.sh
+++ .config/shell/config.d/23-ruby-rvm.sh
@@ -14,18 +14,20 @@ fi
 
 if [[ -s "$RVM_PATH" ]]; then
     export PATH="$RVM_PATH/bin:$PATH" # Add RVM to PATH for scripting
-    rvm() {
-        command -v foo >/dev/null 2>&1 || source "$RVM_PATH/scripts/rvm"
-        exec rvm "$@"
-    }
-elif command -v gem >/dev/null; then
+    source "$RVM_PATH/scripts/rvm"
+    #rvm() {
+        #command -v rvm >/dev/null 2>&1 || source "$RVM_PATH/scripts/rvm"
+        #"$RVM_PATH/bin/rvm" "$@"
+    #}
+fi
+#if command -v gem >/dev/null; then
 
     # apppend user's gems to PATH if directory exists and it is not yet in PATH
-    GEMBIN="$(ruby -e 'print Gem.user_dir')/bin"
-    if [[ $UID -ge 1000 && -d $GEMBIN && -z $(echo $PATH | grep -o $GEMBIN) ]]
-    then
-        export PATH=$PATH:$GEMBIN
-        export GEM_HOME=$GEMBIN
-    fi
-fi
+    #GEMBIN="$(ruby -e 'print Gem.user_dir')/bin"
+    #if [[ $UID -ge 1000 && -d $GEMBIN && -z $(echo $PATH | grep -o $GEMBIN) ]]
+    #then
+        #export PATH=$PATH:$GEMBIN
+        #export GEM_HOME=$GEMBIN
+    #fi
+#fi
 
